3c3077af666d28e9555d4059a107ad04a8c9eb0c
Sergio Ángel Verbo <seranfuen@gmail.com>
2016-11-26 16:41:10 +0100

    Using DB first with Identity
    Added initial entities

--- Chokin/Models/IdentityModels.cs
+++ Chokin/Models/IdentityModels.cs
@@ -21,7 +21,7 @@ namespace Chokin.Models
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
         public ApplicationDbContext()
-            : base("DefaultConnection", throwIfV1Schema: false)
+            : base("ChokinEntitiesIdentity", throwIfV1Schema: false)
         {
         }
 
