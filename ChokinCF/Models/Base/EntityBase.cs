757f30b967360c0bd7a160c6b36cdca6a1d4ab04
Sergio <seranfuen@gmail.com>
2016-12-27 22:17:15 +0100

    Creating T4 infrastructure for creating basic Service Layer / Unit of Work templates for each entity in the model

---
+++ ChokinCF/Models/Base/EntityBase.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ChokinCF.Models
+{
+    public class EntityBase : IId, IAuditable
+    {
+        public int Id
+        {
+            get;
+            set;
+        }
+
+        public DateTime CreatedOn
+        {
+            get;
+            set;
+        }
+
+        public string CreatedUserId
+        {
+            get;
+            set;
+        }
+
+        public ApplicationUser CreatedUser
+        {
+            get;
+            set;
+        }
+
+        public DateTime LastModifiedOn
+        {
+            get;
+            set;
+        }
+
+        public string LastModifiedUserId
+        {
+            get;
+            set;
+        }
+
+        public ApplicationUser LastModifiedUser
+        {
+            get;
+            set;
+        }
+    }
+}
