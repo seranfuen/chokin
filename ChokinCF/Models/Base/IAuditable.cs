9bbad6ab6778448b56c73a2e910a2996cde4934d
Sergio <seranfuen@gmail.com>
2016-12-27 22:17:15 +0100

    Creating T4 infrastructure for creating basic Service Layer / Unit of Work templates for each entity in the model

---
+++ ChokinCF/Models/Base/IAuditable.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChokinCF.Models
+{
+    /// <summary>
+    /// Interface that all auditable entities must implement
+    /// </summary>
+    public interface IAuditable
+    {
+        DateTime CreatedOn
+        {
+            get;
+            set;
+        }
+
+        string CreatedUserId
+        {
+            get;
+            set;
+        }
+
+        ApplicationUser CreatedUser
+        {
+            get;
+            set;
+        }
+
+        DateTime LastModifiedOn
+        {
+            get;
+            set;
+        }
+
+        string LastModifiedUserId
+        {
+            get;
+            set;
+        }
+
+        ApplicationUser LastModifiedUser
+        {
+            get;
+            set;
+        }
+    }
+}
---
+++ ChokinCF/Models/Base/IId.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChokinCF.Models
+{
+    /// <summary>
+    /// An interface that all entities with numeric IDs must implement. Exposes the ID
+    /// </summary>
+    public interface IId
+    {
+        int Id { get; set; }
+    }
+}
