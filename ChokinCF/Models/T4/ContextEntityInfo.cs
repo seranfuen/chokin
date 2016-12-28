ba2ebf406a46042b19d1888ad65df92948b559fa
Sergio <seranfuen@gmail.com>
2016-12-28 22:13:10 +0100

    Now have a working T4 file to generate all the standard properties, interfaces, methods, etc., of the service layer.
    With this, we can easily add more entities to the model and have the T4s generate a basic scaffold of properties and CRUD methods, upon which we will expand to implement the particular business needs

---
+++ ChokinCF/Models/T4/ContextEntityInfo.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ChokinCF.Models.T4
+{
+    public class ContextEntityInfo
+    {
+
+        public ContextEntityInfo(string contextPropertyName, Type entityType)
+        {
+            ContextPropertyName = contextPropertyName;
+            EntityType = entityType;
+        }
+
+        public string ContextPropertyName
+        {
+            get;
+            private set;
+        }
+
+        public Type EntityType
+        {
+            get;
+            private set;
+        }
+
+        public string EntityName
+        {
+            get
+            {
+                return EntityType.Name;
+            }
+        }
+
+        public override string ToString()
+        {
+            return ContextPropertyName + " -> " + EntityType.Name;
+        }
+    }
+}
---
+++ ChokinCF/Models/T4/ModelEntityIterator.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace ChokinCF.Models.T4
+{
+    public class ModelEntityIterator
+    {
+        private Type _contextDbType;
+
+        public ModelEntityIterator(Type contextDbType)
+        {
+            _contextDbType = contextDbType;
+        }
+
+        public List<ContextEntityInfo> ContextEntities
+        {
+            get
+            {
+                var listContextEntities = new List<ContextEntityInfo>();
+                foreach (var property in _contextDbType.GetProperties())
+                {
+                    var typeName = GetTypeName(property.PropertyType.Name);
+                    if (typeName == typeof(DbSet).Name)
+                    {
+                        if (property.PropertyType.GenericTypeArguments != null && property.PropertyType.GenericTypeArguments.Length == 1)
+                        {
+                            listContextEntities.Add(new ContextEntityInfo(property.Name, property.PropertyType.GenericTypeArguments[0]));
+                        }
+                    }
+                }
+                return listContextEntities;
+            }
+        }
+
+        private string GetTypeName(string typeName)
+        {
+            var index = typeName.IndexOf("`");
+            if (index == -1)
+            {
+                return typeName;
+            } else
+            {
+                return typeName.Substring(0, index);
+            }
+        }
+    }
+}
