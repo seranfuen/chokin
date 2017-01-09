81ebff665c39d89afdaa068b29cf289058b4f7ba
Sergio <seranfuen@gmail.com>
2017-01-09 22:15:22 +0100

    Writing bootstrap alert Javascript that works with jQuery

--- ChokinCF/Scripts/app/alerts.js
+++ ChokinCF/Scripts/app/alerts.js
@@ -1,59 +1,79 @@
-﻿var BOOTSTRAP_ALERTS = (function () {
-    var DEFAULT_TIMEOUT_MS = 3000,
-        DEFAULT_POSITION = "top",
-        POSITIONS = ["left", "right", "bottom", "top"];
+﻿// TODO: the animation when showing/hiding should be a parameter too
 
-    var currentAlert = null;
+(function () {
+    'use strict';
 
-    var alerts = {
-        showSuccess: function (message, position, timeout) {
-            var alertMessage = $("<p></p>");
-            timeout = timeout || DEFAULT_TIMEOUT_MS;
-            if (position === undefined || !isValidPosition(position)) {
-                position = DEFAULT_POSITION;
+    var VERSION = "0.0.1",
+        currentAlerts = {}; // each DOM element can have an alert associated. If a timeout is declared and the level is greater than 1, the timeout will not hide the element until it reaches 0
+
+    $.fn.showBootstrapAlert = function (message, options, callback) {
+        /// <summary>
+        /// Displays a bootstrap alert in the element(s) selected
+        /// </summary>
+        /// <param name="message" type="type">the message to show</param>
+        /// <param name="options" type="type">an object containing the following optional properties:
+        ///     timeout: the time in ms until the alert disappears. If undefined or 0, it will never disappear
+        ///     alertType: the type of bootstrap alert (success, info, warning, danger) to display. 
+        ///                If undefined, null or any other value, it will be set to info
+        /// </param>
+        /// <returns type="">jQuery object</returns>
+        if (this.is(":visible")) {
+            this.fadeOut(function () { showAlert(this); });
+        } else {
+            showAlert(this);
+        }
+        return this;
+
+        function showAlert(element) {
+            addAlertLevel(element);
+            $(element).addClass("alert").addClass(getAlertClass(options.alertType)).html(message).fadeIn();
+            if (options["timeout"]) {
+                window.setTimeout(function () {
+                    if (currentAlerts[element] === 1) {
+                        $(element).fadeOut();
+                    }
+                    removeAlertLevel(element);
+                }, options["timeout"]);
             }
-            if (currentAlert !== null) {
-                removeCurrentAlert();
+            else {
+                removeAlertLevel(element);
             }
-            currentAlert = $("<div></div>").addClass("alert").addClass("alert-success").addClass("bootstrap-fixed-alert");
-            currentAlert.hide();
-            setPosition(position);
-            alertMessage.text(message);
-            $("body").prepend(currentAlert);
-            currentAlert.fadeIn();
+        }
+
+        function addAlertLevel(element) {
+            if (currentAlerts[element] === undefined) {
+                currentAlerts[element] = 1;
+            } else {
+                currentAlerts[element]++;
+            }
+        }
+
+        function removeAlertLevel(element) {
+            currentAlerts[element]--;
         }
     };
 
-    function isValidPosition(position) {
-        return POSITIONS.includes(position.toLowerCase());
-    }
-
-    function removeCurrentAlert() {
-        $(document).remove(currentAlert);
-        currentAlert = null;
-    }
-
-    function setPosition(position) {
-        switch (position) {
-            case "left":
-                currentAlert.addClass("bootstrap-fixed-alert-left");
-                break;
-            case "right":
-                currentAlert.addClass("bootstrap-fixed-alert-right");
-                break;
-            case "top":
-                currentAlert.addClass("bootstrap-fixed-alert-top");
-                break;
-            case "bottom":
-                currentAlert.addClass("bootstrap-fixed-alert-bottom");
-                break;
+    function getAlertClass(alertType) {
+        alertType = alertType || "";
+        switch (alertType.toLowerCase()) {
+            case "success":
+                return "alert-success";
+            case "warning":
+                return "alert-warning";
+            case "danger":
+                return "alert-danger";
+            default:
+                return "alert-info";
         }
     }
-
-    return alerts;
-
 })();
 
-$(document).ready(function () {
-    BOOTSTRAP_ALERTS.showSuccess("PROBANDO PROBANDO", "top");
-});
+//$(document).ready(function () {
+//    $("#alert-box").showBootstrapAlert("The request was successfully processed", { alertType: "success", timeout : 5000 });
+//});
+
+//$(document).ready(function () {
+//    window.setTimeout(function () {
+//        $("#alert-box").showBootstrapAlert("Server error", { alertType: "danger", timeout: 6000 });
+//    }, 3000);
+//});
