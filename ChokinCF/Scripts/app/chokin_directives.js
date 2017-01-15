174dc6bf3313d8588507e7c15474b2ca65d2aa82
Sergio <seranfuen@gmail.com>
2017-01-15 20:00:10 +0100

    Refactoring

---
+++ ChokinCF/Scripts/app/chokin_directives.js
@@ -0,0 +1,52 @@
+﻿(function () {
+    'use strict';
+    var app = angular.module("chokin");
+    app.directive("focusOn", function ($timeout) {
+        return {
+            restrict: "A",
+            link: function ($scope, $element, $attr) {
+                $scope.$watch($attr.focusOn, function (focusVal) {
+                    $timeout(function () {
+                        focusVal ? $element[0].select() :
+                            $element[0].blur();
+                    });
+                });
+            }
+        };
+    });
+
+    app.directive("escapePressed", function () {
+        return function (scope, element, attributes) {
+            $(element).keyup(function (event) {
+                if (event.keyCode === 27) {
+                    scope.$apply(function ()
+                    {
+                        scope.$eval(attributes.escapePressed)
+                    });
+                }
+            });
+        };
+    });
+
+    app.directive("enterPressed", function () {
+        return function (scope, element, attributes) {
+            $(element).keyup(function (event) {
+                if (event.keyCode === 13) {
+                    scope.$apply(function () {
+                        scope.$eval(attributes.enterPressed);
+                    });
+                }
+            });
+        };
+    });
+
+    app.directive("tooltip", function () {
+        function link(scope, element, attributes) {
+            $(element).tooltip();
+        }
+
+        return {
+            link: link
+        }
+    });
+})();
--- ChokinCF/Scripts/app/spreadsheet_entity_editor.js
+++ ChokinCF/Scripts/app/spreadsheet_entity_editor.js
@@ -1,7 +1,7 @@
 ﻿var serfuen = serfuen || {};
 
 (function () {
-    'use strict';
+    "use strict";
     serfuen.utils = {
         combinePath: function () {
             var elements = Array.from(arguments);
@@ -11,9 +11,9 @@
 })();
 
 (function () {
-    'use strict';
-    var app = angular.module("angularTest", []);
-    app.controller("SpreadsheetModeEdit", function ($scope, $http, $attrs) {
+    "use strict";
+    var app = angular.module("chokin");
+    app.controller("chokin.SpreadsheetModeEdit", function ($scope, $http, $attrs) {
         var isAdding = false,
             currentEditingEntity,
             newEntityId = 0,
@@ -23,35 +23,38 @@
             combinePath = serfuen.utils.combinePath;
 
         repositoryBaseUrl = "/" + combinePath("api", $attrs.entityRepository);
-        $scope.isEditing = []; // associative array Id --> boolean that establishes whether the current entity is being edited
-        $scope.getEditingClass = function (entityId) { // tests if the entity with the ID given is being edited
-            return $scope.isEditing[entityId] ? "entity-row-editing" : "entity-row";
-        };
 
-        var endEdit = function (commitData, callbackOnSaved) {
+        var endEdit = function (commitData, callbackOnEditionEnded) {
             /// <summary>
             /// Causes the edition on the current row to end, returning to a clean state. If commitData is true, it will attempt to save the data. If false,
             /// it will roll back all changes.
-            /// 
             /// The edit state will not be switched off if commitData is true and the data is not valid. In this case, an error will be shown and the user will be able
             /// to correct the data, or discard the data
             /// </summary>
             /// <param name="commitData" type="type">if true, all changes will be saved. If false, all changes will be rolled back</param>
             if (currentEditingId !== null) {
                 if (commitData === true && (isAdding === true || !angular.equals(currentEditingEntity, getEntity(currentEditingId)))) {
-                    saveCurrent(currentEditingId, callbackOnSaved);
+                    saveCurrent(currentEditingId, callbackOnEditionEnded);
                 } else if (isAdding === true) { // remove entity being added
                     removeEntity(currentEditingId);
+                    exitEditMode();
                 } else { // restore entity being edited
                     var editing = getEntity(currentEditingId);
                     angular.copy(currentEditingEntity, editing);
-                    $scope.isEditing[currentEditingId] = false;
+                    exitEditMode();
+                    if (typeof callbackOnEditionEnded === "function") {
+                        callbackOnEditionEnded();
+                    }
                 }
-                $scope.$apply();
             }
-        }
+        };
 
-        $scope.endEdit = endEdit;
+        function exitEditMode() {
+            $scope.isEditing[currentEditingId] = false;
+            currentEditing = null;
+            currentEditingId = null;
+            isAdding = false;
+        }
 
         function removeEntity(id) {
             var indexAdding = $scope.currencies.findIndex(function (element) {
@@ -62,7 +65,7 @@
             $scope.currencies.splice(indexAdding, 1);
         }
 
-        function saveCurrent(id, callbackOnSaved) {
+        function saveCurrent(id, callbackOnEditionEnded) {
             var savingEntity = getEntity(id);
             var jsonEntity =  angular.toJson(savingEntity);
             if (id === newEntityId) {
@@ -73,10 +76,10 @@
 
             function saveCurrentSuccess(response) {
                 // show OK! message
-                currentEditing = currentEditingId = null;
-                isAdding = false;
-                $scope.isEditing[currentEditingId] = false;
-                callbackOnSaved();
+                exitEditMode();
+                if (typeof callbackOnEditionEnded === "function") {
+                    callbackOnEditionEnded();
+                }
             }
 
             function saveCurrentFailure(response) {
@@ -101,7 +104,6 @@
             return firstSeparatorIndex >= 0 ? path.substr(firstSeparatorIndex + 1) : path;
         }
 
-
         function getEntity(id) {
             return $scope.currencies.find(function (entity) {
                 return entity.Id === id;
@@ -123,30 +125,11 @@
             }
         });
 
-        $scope.startEdit = function ($event, id) {
-            /// <summary>
-            /// Sets the row edit mode on. Allows modifying the data on the row whose Id is passed. If another row is being edited,
-            /// the changes will be commited
-            /// </summary>
-            /// <param name="$event" type="type">the event data</param>
-            /// <param name="id" type="type">the Id of the row to modify</param>
-            var delegateTarget = $event.target;
-            if (currentEditingId !== null && currentEditingId !== id) {
-                endEdit(true, function () {
-                    setEditMode();
-                });
-            }
-            else if (currentEditingId === null || currentEditingId !== id) {
-                setEditMode();
-            }
-
-            function setEditMode() {
-                editMode(id);
-                currentEditing = delegateTarget;
-                $event.stopPropagation();
-                $scope.modelStatus = [];
-            }
-        };
+        function setAdding() {
+            $scope.modelStatus = [];
+            isAdding = true;
+            setEditing(newEntityId);
+        }
 
         function editMode(id) {
             setEditing(id);
@@ -203,56 +186,35 @@
             $scope.currencies =  angular.fromJson(data.data);
         });
 
-        function setAdding() {
-            $scope.modelStatus = [];
-            isAdding = true;
-            setEditing(newEntityId);
-        }
-    });
-
-    // DIRECTIVES
-
-    app.directive("focusOn", function($timeout) {
-        return {
-            restrict : "A",
-            link : function($scope,$element,$attr) {
-                $scope.$watch($attr.focusOn, function(focusVal) {
-                    $timeout(function() {
-                        focusVal ? $element[0].select() :
-                            $element[0].blur();
-                    });
+        $scope.startEdit = function ($event, id) {
+            /// <summary>
+            /// Sets the row edit mode on. Allows modifying the data on the row whose Id is passed. If another row is being edited,
+            /// the changes will be commited
+            /// </summary>
+            /// <param name="$event" type="type">the event data</param>
+            /// <param name="id" type="type">the Id of the row to modify</param>
+            var delegateTarget = $event.target;
+            if (currentEditingId !== null && currentEditingId !== id) {
+                endEdit(true, function () {
+                    setEditMode();
                 });
             }
+            else if (currentEditingId === null || currentEditingId !== id) {
+                setEditMode();
+            }
+
+            function setEditMode() {
+                editMode(id);
+                currentEditing = delegateTarget;
+                $event.stopPropagation();
+                $scope.modelStatus = [];
+            }
         };
-    });
 
-    app.directive("escapePressed", function() {
-        return function(scope, element, attributes) {
-            $(element).keyup(function (event) {
-                if (event.keyCode === 27) {
-                    scope.$eval(attributes.escapePressed);
-                }
-            });
+        $scope.endEdit = endEdit;
+        $scope.isEditing = []; // associative array Id --> boolean that establishes whether the current entity is being edited
+        $scope.getEditingClass = function (entityId) { // tests if the entity with the ID given is being edited
+            return $scope.isEditing[entityId] ? "entity-row-editing" : "entity-row";
         };
-    });
-
-    app.directive("enterPressed", function () {
-        return function (scope, element, attributes) {
-            $(element).keyup(function (event) {
-                if (event.keyCode === 13) {
-                    scope.$eval(attributes.enterPressed);
-                }
-            });
-        };
-    });
-
-    app.directive("tooltip", function () {
-        function link(scope, element, attributes) {
-            $(element).tooltip();
-        }
-
-        return {
-            link: link
-        }
     });
 })();
